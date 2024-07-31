22b17d74e81c28e625888f53e6306c8c3dba2198
guanciottaman <italia.matteo00@gmail.com>
2024-07-31 22:48:17 +0200

    Changed the views management using FleetingViews

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import flet as ft
 from flet_core.canvas.canvas import ControlEvent
+import FleetingViews as fleetingviews
 import requests
 from bs4 import BeautifulSoup
 
+import asyncio
 from dataclasses import dataclass
 from datetime import datetime
 import webbrowser
 
-
-#RECYCLING_TYPES_NAMES: list[str] = ["Organico, umido", "Carta e Cartone", "Plastica e Alluminio", "Vetro",
-#    "Secco non riciclabile", "Pannolini, pannoloni, traverse"]
 
 WEEKDAYS_ITA: list[str] = ["Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica"]
 
@@ -31,9 +30,17 @@ def main(page: ft.Page):
     vetro_img = ft.Image(src="vetro.png", width=96, height=96)
     indifferenziata_img = ft.Image(src="indifferenziata.png", width=96, height=96)
     pannolini_img = ft.Image(src="pannolini.png", width=96, height=96)
+    
+    REC_TYPES_TO_IMGS = {
+        "Organico, umido": organico_img,
+        "Carta e Cartone": carta_img,
+        "Plastica e Alluminio": plastica_img,
+        "Vetro": vetro_img,
+        "Secco non riciclabile": indifferenziata_img,
+        "Pannolini, pannoloni, traverse": pannolini_img
+    }
 
-    def create_cards_news() -> list[ft.Card]:
-        news = load_news()
+    def create_cards_news(news: list[New]) -> list[ft.Card]:
         cards = []
         for new in news:
             card = ft.Card(
@@ -63,7 +70,7 @@ def main(page: ft.Page):
             cards.append(card)
         return cards
 
-    def load_news() -> list[New]:
+    async def load_news() -> list[New]:
         news_lst: list = []
         r = requests.get("https://www.comune.floridia.sr.it/categoria/news/")
         content = r.text
@@ -109,69 +116,16 @@ def main(page: ft.Page):
                 recycle_types.append(recycling_type)
         return recycle_types
 
-    def home_view():
-        page.views.append(
-            ft.View(
-                "/",
-                controls=[
-                    ft.AppBar(
-                        leading=hamburger_btn,
-                        title=ft.Text("Floridia One", font_family="Montserrat")
-                    ),
-                    ft.Text("Home View", font_family="Montserrat")
-                ]
-            )
-        )
-        page.update()
-
-    def news_view():
-        print("Creating news view...")
-        page.views.append(
-            ft.View(
-                "/news",
-                controls=[
-                    ft.AppBar(
-                        leading=hamburger_btn,
-                        title=ft.Text("Floridia One", font_family="Montserrat")
-                    ),
-                    ft.GridView(
-                        controls=create_cards_news(), # pyright: ignore
-                        expand=1,
-                        runs_count=3,
-                        max_extent=600,
-                        child_aspect_ratio=1.5,
-                        spacing=5,
-                        run_spacing=5,
-                    )
-                ]
-            )
-        )
-        page.update()
-            
-    def differenziata_view():
-        print(load_today_recycle_types())
-        page.views.append(
-            ft.View(
-                "/differenziata",
-                controls=[
-                    ft.AppBar(
-                        leading=hamburger_btn,
-                        title=ft.Text("Floridia One", font_family="Montserrat")
-                    ),
-                    ft.Text(f"Oggi devi uscire:"),
-                ]
-            )
-        )
-        page.update()
-
     def handle_change(e: ControlEvent):
         match e.control.selected_index:
             case 0:
-                page.go("/")
+                fv.view_go("/home")
             case 1:
-                page.go("/news")
+                fv.view_go("/news")
             case 2:
-                page.go("/differenziata")
+                fv.view_go("/differenziata")
+            case 3:
+                fv.view_go("/eventi")
 
     drawer = ft.NavigationDrawer(
         on_change=handle_change,
@@ -192,40 +146,106 @@ def main(page: ft.Page):
                 icon_content=ft.Icon(ft.icons.RECYCLING),
                 label="Differenziata",
                 selected_icon=ft.icons.RECYCLING_OUTLINED,
+            ),
+            ft.NavigationDrawerDestination(
+                icon_content=ft.Icon(ft.icons.EVENT),
+                label="Eventi",
+                selected_icon=ft.icons.RECYCLING_OUTLINED,
             )
         ]
     )
 
-    
-    
+    def show_drawer(e):
+        page.open(drawer)
 
     hamburger_btn = ft.IconButton(
         icon=ft.icons.MENU,
-        on_click=lambda e: page.open(drawer)
+        on_click=show_drawer
     )
 
-    def route_change(route):
-        page.views.clear()
-        home_view()
-        if page.route == "/news":
-            news_view()
-        elif page.route == "/differenziata":
-            differenziata_view()
-
-    def view_pop(view):
-        page.views.pop()
-        top_view = page.views[-1]
-        page.go(top_view.route) # pyright: ignore
+    view_definitions = {
+        "home": {
+            'vertical_alignment': ft.MainAxisAlignment.CENTER,
+            'horizontal_alignment': ft.CrossAxisAlignment.CENTER
+        },
+        "news": {
+            "vertical_alignment": ft.MainAxisAlignment.CENTER,
+            "horizontal_alignment": ft.CrossAxisAlignment.CENTER
+        },
+        "differenziata": {
+            'vertical_alignment': ft.MainAxisAlignment.CENTER,
+            'horizontal_alignment': ft.CrossAxisAlignment.CENTER
+        },
+        "eventi": {
+            'vertical_alignment': ft.MainAxisAlignment.CENTER,
+            'horizontal_alignment': ft.CrossAxisAlignment.CENTER
+        }
+    }
     
-    page.on_route_change = route_change
-    page.on_view_pop = view_pop
+    fv = fleetingviews.create_views(view_definitions=view_definitions, page=page)
+    
+    fv.append("home", controls=[
+            ft.AppBar(
+                leading=hamburger_btn,
+                title=ft.Text("Floridia One", font_family="Montserrat")
+            ),
+            ft.Text("Home View", font_family="Montserrat")
+        ]
+    )
+    
+    async def append_to_news():
+        news = await load_news()
+        cards = create_cards_news(news)
+        fv.append("news", controls=[
+                ft.AppBar(
+                    leading=hamburger_btn,
+                    title=ft.Text("Floridia One", font_family="Montserrat")
+                ),
+                ft.GridView(
+                    controls=cards, # pyright: ignore
+                    expand=1,
+                    runs_count=3,
+                    max_extent=600,
+                    child_aspect_ratio=1.5,
+                    spacing=5,
+                    run_spacing=5,
+                )
+            ]
+        )
+    asyncio.run(append_to_news())
 
-    page.appbar = ft.AppBar(
+    today_recycle_types = load_today_recycle_types()
+    rec_types_cards = []
+
+    for rec_type in today_recycle_types:
+        rec_card = ft.Card(
+            ft.Column([
+                REC_TYPES_TO_IMGS[rec_type],
+                ft.Text(rec_type, font_family="Montserrat")
+            ])
+        )
+        rec_types_cards.append(rec_card)
+
+    fv.append("differenziata", controls=[
+       ft.AppBar(
+           leading=hamburger_btn,
+           title=ft.Text("Floridia One", font_family="Montserrat")
+       ),
+       ft.Text(f"Oggi devi uscire:"),
+       
+   ])
+    
+
+    page.drawer = drawer
+
+    appbar = ft.AppBar(
         leading=hamburger_btn,
         title=ft.Text("Floridia One", font_family="Montserrat")
     )
-    page.drawer = drawer
+
+    page.appbar = appbar
+
     page.update()
-    page.go(page.route)
+    fv.view_go("home")
 
 ft.app(main, name="Floridia One", assets_dir="assets")
